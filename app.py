b220b414270261de416e560c59d6d3a992aa8e78
YathinChandra64 <144458444+YathinChandra64@users.noreply.github.com>
2023-09-29 11:53:54 +0000

    added html and deployed on replit

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
-def hello_world():
-  return "i am new replit.com"
-
+def main():
+  return render_template('main.html')
+@app.route("/Login.html/")
+def Login():
+  return render_template('Login.html')
+@app.route("/Products.html/")
+def Products():
+  return render_template('Products.html')
+@app.route("/Aboutus.html/")
+def Aboutus():
+  return render_template('Aboutus.html')
+@app.route("/main.html/")
+def Home():
+  return render_template('main.html')
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
